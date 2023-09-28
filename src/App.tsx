2d194bf7e75e1399b2abea1cf70d5fd8c8b3e7d0
Dane Macaulay <dane.macaulay@gmail.com>
2023-09-27 19:53:12 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,13 @@
-// @ts-nocheck
-import { useState, useEffect } from "react";
-import { createClient } from "@supabase/supabase-js";
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import Session from "./Session";
+import React from "react";
+import "./App.css";
+import UploadComponent from "./components/UploadComponent";
 
-const views: string[] = [
-  "sign_in",
-  "sign_up",
-  "magic_link",
-  "forgotten_password",
-  "update_password",
-  "verify_otp",
-];
-
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY,
-);
-
-export default function App() {
-  const [session, setSession] = useState(null);
-  const [view, setView] = useState(views[0]);
-  const searchParams = new URLSearchParams(window.location.search);
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session);
-    });
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session);
-    });
-
-    return () => subscription.unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    let v = searchParams.get("view");
-    if (views.includes(v)) {
-      setView(v);
-    }
-  }, []);
-
-  if (!session) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "500px",
-        }}
-      >
-        <div
-          style={{
-            width: "350px",
-          }}
-        >
-          <Auth
-            supabaseClient={supabase}
-            appearance={{ theme: ThemeSupa }}
-            view={view}
-            providers={[]}
-          />
-        </div>
-      </div>
-    );
-  } else {
-    return <Session supabase={supabase} />;
-  }
+function App() {
+  return (
+    <div className="App">
+      <UploadComponent />
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/UploadComponent.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import axios from "axios";
+import Dropzone from "react-dropzone";
+import Typography from "@mui/joy/Typography";
+import Button from "@mui/joy/Button";
+import Box from "@mui/joy/Box";
+import Alert from "@mui/joy/Alert";
+import Link from "@mui/joy/Link";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+function ImageList({ images, uploaded }) {
+  return (
+    <Box>
+      {uploaded && (
+        <Box sx={{ display: "inline-block", overflowX: "hidden", width: 200 }}>
+          <AspectRatio ratio="9/16">
+            <Link href="#">
+              <img src={uploaded} alt="uploaded" />
+            </Link>
+          </AspectRatio>
+          <Typography level="h6">Uploaded</Typography>
+        </Box>
+      )}
+      {images?.map((image, index) => (
+        <Box
+          key={index}
+          sx={{ display: "inline-block", overflowX: "hidden", width: 200 }}
+        >
+          <AspectRatio ratio="9/16">
+            <Link
+              href={image.wiki_url ? image.wiki_url : "#"}
+              target="_blank"
+              disabled={!image.wiki_url}
+            >
+              <img src={image.image} alt={image.name} />
+            </Link>
+          </AspectRatio>
+          <Typography level="h6" noWrap>
+            {image.name} - {parseFloat(image.score).toFixed(2)}
+          </Typography>
+        </Box>
+      ))}
+    </Box>
+  );
+}
+
+const UploadComponent = () => {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [results, setResults] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const onDrop = (acceptedFiles) => {
+    setUploadedFiles(acceptedFiles);
+    uploadFiles(acceptedFiles);
+  };
+
+  const uploadFiles = async (files) => {
+    setError("");
+    setResults({});
+    setLoading(true);
+    const formData = new FormData();
+    files.forEach((file) => {
+      formData.append("files", file);
+    });
+
+    try {
+      const r = await axios.post("http://34.30.142.181/faces", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      setResults(r.data);
+      setError("");
+      // Handle successful upload
+    } catch (error) {
+      setError(error);
+      // Handle upload error
+    }
+    setLoading(false);
+  };
+
+  const file = uploadedFiles?.[0];
+  const uploaded = file ? URL.createObjectURL(file) : false;
+
+  return (
+    <Box>
+      <Box sx={{ m: 2 }}>
+        <Dropzone onDrop={onDrop}>
+          {({ getRootProps, getInputProps }) => (
+            <Button
+              {...getRootProps()}
+              loading={loading}
+              variant="outlined"
+              sx={{ width: "100%", height: "100px" }}
+            >
+              <input {...getInputProps()} />
+              <p>Drag and drop a file here, or click to select a file</p>
+            </Button>
+          )}
+        </Dropzone>
+      </Box>
+      {error?.message?.length > 0 && (
+        <Box sx={{ m: 2 }}>
+          <Alert variant="outlined" color="danger">
+            oops! something went wrong: {error.message}
+          </Alert>
+        </Box>
+      )}
+      <ImageList images={results.wiki} uploaded={uploaded} />
+    </Box>
+  );
+};
+
+export default UploadComponent;
