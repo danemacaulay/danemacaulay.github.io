810dcefb8998d84ab71c7f4aec49f4ea4327ef16
Dane Macaulay <dane.macaulay@gmail.com>
2023-09-28 10:28:46 -0500

    update

--- src/components/UploadComponent.jsx
+++ src/components/UploadComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropzone from "react-dropzone";
 import Typography from "@mui/joy/Typography";
@@ -6,7 +6,9 @@ import Button from "@mui/joy/Button";
 import Box from "@mui/joy/Box";
 import Alert from "@mui/joy/Alert";
 import Link from "@mui/joy/Link";
+import IconButton from "@mui/joy/IconButton";
 import AspectRatio from "@mui/joy/AspectRatio";
+import CloseIcon from "@mui/icons-material/Close";
 
 function ImageList({ images, uploaded, getSimilar }) {
   return (
@@ -18,7 +20,7 @@ function ImageList({ images, uploaded, getSimilar }) {
               <img src={uploaded} alt="uploaded" />
             </Link>
           </AspectRatio>
-          <Typography level="h6">Uploaded</Typography>
+          <Typography level="h6">Source image</Typography>
         </Box>
       )}
       {images?.map((image, index) => {
@@ -58,12 +60,41 @@ const UploadComponent = () => {
   const [error, setError] = useState("");
   const [seedImage, setSeedImage] = useState("");
 
+  const demos = [
+    {
+      name: "Elon Musk",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg/440px-Elon_Musk_Colorado_2022_%28cropped2%29.jpg",
+    },
+    {
+      name: "Donald Trump",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Donald_Trump_official_portrait.jpg/440px-Donald_Trump_official_portrait.jpg",
+    },
+    {
+      name: "Bruce Springsteen",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Bruce_Springsteen_-_Roskilde_Festival_2012.jpg/440px-Bruce_Springsteen_-_Roskilde_Festival_2012.jpg",
+    },
+    {
+      name: "Cher",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Cher_-_Casablanca.jpg/1024px-Cher_-_Casablanca.jpg",
+    },
+    {
+      name: "Taylor Swift",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png/440px-Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png",
+    },
+  ];
+
   const onDrop = (acceptedFiles) => {
     setUploadedFiles(acceptedFiles);
     uploadFiles(acceptedFiles);
-    const file = uploadedFiles?.[0];
+    const file = acceptedFiles?.[0];
     const url = URL.createObjectURL(file);
     setSeedImage(url);
+  };
+
+  const clear = () => {
+    setUploadedFiles();
+    setSeedImage();
+    setResults({});
   };
 
   const uploadFiles = async (files) => {
@@ -71,7 +102,7 @@ const UploadComponent = () => {
     setResults({});
     setLoading(true);
     const formData = new FormData();
-    files.forEach((file) => {
+    Array.from(files).forEach((file) => {
       formData.append("files", file);
     });
 
@@ -104,9 +135,16 @@ const UploadComponent = () => {
     setLoading(false);
   };
 
+  useEffect(() => {
+    window.addEventListener("paste", (e) => {
+      if (e.clipboardData.files.length === 0) return;
+      uploadFiles(e.clipboardData.files);
+    });
+  }, []);
+
   return (
     <Box>
-      <Box sx={{ m: 2 }}>
+      <Box sx={{ m: 2, mr: 6, ml: 6 }}>
         <Dropzone onDrop={onDrop}>
           {({ getRootProps, getInputProps }) => (
             <Button
@@ -120,6 +158,20 @@ const UploadComponent = () => {
             </Button>
           )}
         </Dropzone>
+        {results?.wiki?.length > 0 && (
+          <Box
+            sx={{
+              position: "absolute",
+              top: 4,
+              right: 4,
+              background: "white",
+            }}
+          >
+            <IconButton aria-label="Close" variant="outlined" onClick={clear}>
+              <CloseIcon />
+            </IconButton>
+          </Box>
+        )}
       </Box>
       {error?.message?.length > 0 && (
         <Box sx={{ m: 2 }}>
@@ -133,6 +185,23 @@ const UploadComponent = () => {
         uploaded={seedImage}
         getSimilar={getSimilar}
       />
+      {!seedImage && (
+        <Box>
+          <Typography sx={{ mb: 1 }}>Or try some of these:</Typography>
+          {demos.map((d) => {
+            return (
+              <Button
+                variant="outlined"
+                sx={{ mr: 1 }}
+                key={d.name}
+                onClick={() => getSimilar(d.url)}
+              >
+                {d.name}
+              </Button>
+            );
+          })}
+        </Box>
+      )}
     </Box>
   );
 };
